019dbd000d201fe91b65eb925c45a26251e8bb7c
eunjung04 <peunj04@gmail.com>
2020-04-22 21:53:44 +0900

    피자가게 상세화면 액티비티 추가

---
+++ app/src/main/java/com/example/pizzastoreapp/Main2Activity.kt
@@ -0,0 +1,12 @@
+package com.example.pizzastoreapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main2Activity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
