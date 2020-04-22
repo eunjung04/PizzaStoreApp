a72a297f34b1961ae9ffc43a94b1369c01b3d9f1
eunjung04 <peunj04@gmail.com>
2020-04-22 21:05:34 +0900

    가게목록 어댑터 뼈대 작업

---
+++ app/src/main/java/com/example/pizzastoreapp/adapter/StoreAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.pizzastoreapp.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.example.pizzastoreapp.datas.Store
+
+class StoreAdapter(val context: Context, resId:Int, list:ArrayList<Store>) : ArrayAdapter<Store>(context,resId,list) {
+
+    val mContext=context
+    val mList=list
+    val inf=LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val tempRow=convertView
+
+      tempRow?.let {
+
+      }.let {
+          tempRow=inf.inflate(R.layout.store_list_item,null)
+      }
+
+        val row=tempRow!!
+
+        return row
+
+    }
+}
